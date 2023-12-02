067ac9f43bc1d922981fafbf7f40b931d0f6c10b
Baharudin Abdulloh Mun'im <abdullohbahar@gmail.com>
2023-12-03 05:21:46 +0700

    add level kepuasan

--- resources/views/guest/kritik-saran.blade.php
+++ resources/views/guest/kritik-saran.blade.php
@@ -66,38 +66,65 @@
                     <div class="card-body">
                         <form action="">
                             <div class="row mt-3">
-                                <div class="col-sm-12 col-md-2 text-right">
-                                    <label class="mt-2 ml-sm-4" for=""><b>Nama</b></label>
+                                <div class="col-sm-12 col-md-12 col-lg-2 text-lg-right">
+                                    <label class="mt-2 ml-lg-4" for=""><b>Nama</b></label>
                                 </div>
-                                <div class="col-sm-12 col-md-10">
+                                <div class="col-sm-12 col-md-12 col-lg-10">
                                     <input type="text" name="nama" class="form-control" style="width: 100%"
                                         id="">
                                 </div>
                             </div>
                             <div class="row mt-3">
-                                <div class="col-sm-12 col-md-2 text-right">
-                                    <label class="mt-2 ml-sm-4" for=""><b>Email</b></label>
+                                <div class="col-sm-12 col-md-12 col-lg-2 text-lg-right">
+                                    <label class="mt-2 ml-lg-4" for=""><b>Email</b></label>
                                 </div>
-                                <div class="col-sm-12 col-md-10">
+                                <div class="col-sm-12 col-md-12 col-lg-10">
                                     <input type="email" name="email" class="form-control" style="width: 100%"
                                         id="">
                                 </div>
                             </div>
                             <div class="row mt-3">
-                                <div class="col-sm-12 col-md-2 text-right">
-                                    <label class="mt-2 ml-sm-4" for=""><b>No Telepon</b></label>
+                                <div class="col-sm-12 col-md-12 col-lg-2 text-lg-right">
+                                    <label class="mt-2 ml-lg-4" for=""><b>No Telepon</b></label>
                                 </div>
-                                <div class="col-sm-12 col-md-10">
+                                <div class="col-sm-12 col-md-12 col-lg-10">
                                     <input type="text" name="no_telp" class="form-control" style="width: 100%"
                                         id="">
                                 </div>
                             </div>
                             <div class="row mt-3">
-                                <div class="col-sm-12 col-md-2 text-right">
-                                    <label class="mt-2 ml-sm-4" for=""><b>Kritik dan Saran</b></label>
+                                <div class="col-sm-12 col-md-12 col-lg-2 text-lg-right">
+                                    <label class="mt-2 ml-lg-4" for=""><b>Kritik dan Saran</b></label>
                                 </div>
-                                <div class="col-sm-12 col-md-10">
+                                <div class="col-sm-12 col-md-12 col-lg-10">
                                     <textarea name="kritik_dan_saran" class="form-control" id=""></textarea>
+                                </div>
+                            </div>
+                            <div class="row mt-3">
+                                <div class="col-sm-12 col-md-12 col-lg-2 text-lg-right">
+                                    <label class="mt-2 ml-lg-4" for=""><b>Tingkat Kepuasan</b></label>
+                                </div>
+                                <div class="col-sm-12 col-md-12 col-lg-10">
+                                    <div class="custom-control custom-radio">
+                                        <input type="radio" id="sangat-puas" value="sangat-puas" name="tingkat-kepuasan"
+                                            class="custom-control-input">
+                                        <label class="custom-control-label" for="sangat-puas">Sangat Puas</label>
+                                    </div>
+                                    <div class="custom-control custom-radio">
+                                        <input type="radio" id="puas" value="puas" name="tingkat-kepuasan"
+                                            class="custom-control-input">
+                                        <label class="custom-control-label" for="puas">Puas</label>
+                                    </div>
+                                    <div class="custom-control custom-radio">
+                                        <input type="radio" id="kurang-puas" value="kurang-puas" name="tingkat-kepuasan"
+                                            class="custom-control-input">
+                                        <label class="custom-control-label" for="kurang-puas">Kurang Puas</label>
+                                    </div>
+                                    <div class="custom-control custom-radio">
+                                        <input type="radio" id="tidak-puas" value="tidak-puas" name="tingkat-kepuasan"
+                                            class="custom-control-input">
+                                        <label class="custom-control-label" for="tidak-puas">Tidak Puas</label>
+                                    </div>
                                 </div>
                             </div>
                             <div class="row mt-3">
