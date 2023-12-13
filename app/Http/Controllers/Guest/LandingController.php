844ab65969c6042e5708bd95ed9333765bb68502
Baharudin Abdulloh Mun'im <abdullohbahar@gmail.com>
2023-12-13 10:14:05 +0700

    add grafik pendaftaran

--- app/Http/Controllers/Guest/LandingController.php
+++ app/Http/Controllers/Guest/LandingController.php
@@ -2,19 +2,37 @@
 
 namespace App\Http\Controllers\Guest;
 
-use App\Http\Controllers\Controller;
-use App\Models\Regulasi;
 use App\Models\Slider;
+use App\Models\Regulasi;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
 
 class LandingController extends Controller
 {
     public function index()
     {
         $sliders = Slider::all();
+        $year = date('Y');
+
+        $pendaftaranTahunan = DB::table('pendaftarans')
+            ->select(DB::raw("DATE_FORMAT(tglpendaftaran, '%Y-%m') AS bulan_pendaftaran"), DB::raw('COUNT(*) AS total_pendaftar'))
+            ->whereYear('tglpendaftaran', $year)
+            ->groupBy(DB::raw("DATE_FORMAT(tglpendaftaran, '%Y-%m')"))
+            ->orderBy('bulan_pendaftaran', 'ASC')
+            ->get();
+
+        // Inisialisasi array untuk menyimpan data yang akan digunakan di grafik Highcharts
+        $dataPendaftarTahunan = [];
+
+        // Mengisi array dataPendaftarTahunan dengan hasil query
+        foreach ($pendaftaranTahunan as $result) {
+            $dataPendaftarTahunan[] = (int)$result->total_pendaftar;
+        }
 
         $data = [
-            'sliders' => $sliders
+            'sliders' => $sliders,
+            'dataPendaftarTahunan' => json_encode($dataPendaftarTahunan)
         ];
 
         return view('guest.landing', $data);
