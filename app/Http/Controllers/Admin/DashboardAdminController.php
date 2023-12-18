6c7bcb73507bc6ccccc008c31a922c06436fd23c
Baharudin Abdulloh Mun'im <abdullohbahar@gmail.com>
2023-12-18 21:35:59 +0700

    add grafik in dashboard admin

--- app/Http/Controllers/Admin/DashboardAdminController.php
+++ app/Http/Controllers/Admin/DashboardAdminController.php
@@ -2,17 +2,145 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
 
 class DashboardAdminController extends Controller
 {
     public function index()
     {
+        $year = date('Y');
+        $month = date('m');
+
+        $pendaftarBulanan = $this->pendaftaranBulanan();
+
+        $pendaftaranHarian = $this->pendaftaranHarian();
+
+        $pendaftaranTahunan = $this->pendaftaranTahunan();
+
         $data = [
-            'active' => 'dashboard'
+            'active' => 'dashboard',
+            'dataPendaftarBulanan' => json_encode($pendaftarBulanan),
+            'datapendaftarHarianCategories' => $pendaftaranHarian['categories'],
+            'pendaftaranHarian' => $pendaftaranHarian['pendaftaranHarian'],
+            'dataPendaftarTahunan' => json_encode($pendaftaranTahunan),
+            'year' => $year,
+            'month' => $month
         ];
 
         return view('admin.dashboard.index', $data);
+    }
+
+    public function pendaftaranHarian()
+    {
+        $year = date('Y');
+        $month = date('m');
+
+        // Ambil data dari database
+        $pendaftaranHarian = DB::table('pendaftarans')
+            ->select(DB::raw("DATE_FORMAT(tglpendaftaran, '%Y-%m-%d') AS tanggal_pendaftaran"), DB::raw('COALESCE(COUNT(*), 0) AS total_pendaftar'))
+            ->whereYear('tglpendaftaran', $year)
+            ->whereMonth('tglpendaftaran', $month)
+            ->groupBy(DB::raw("DATE_FORMAT(tglpendaftaran, '%Y-%m-%d')"))
+            ->orderBy('tanggal_pendaftaran', 'ASC')
+            ->get();
+
+        // Buat array dengan semua tanggal dalam rentang bulan ini
+        $allDatesInMonth = [];
+        $daysInMonth = cal_days_in_month(CAL_GREGORIAN, $month, $year);
+        for ($day = 1; $day <= $daysInMonth; $day++) {
+            $date = sprintf("%04d-%02d-%02d", $year, $month, $day);
+            $allDatesInMonth[] = $date;
+        }
+
+        // Ubah hasil query menjadi associative array
+        foreach ($pendaftaranHarian as $item) {
+            $pendaftaranArray[$item->tanggal_pendaftaran] = $item->total_pendaftar;
+        }
+
+        // Isi nilai 0 untuk tanggal yang tidak ada dalam hasil query
+        foreach ($allDatesInMonth as $date) {
+            if (!isset($pendaftaranArray[$date])) {
+                $pendaftaranHarian->push((object) ['tanggal_pendaftaran' => $date, 'total_pendaftar' => 0]);
+            }
+        }
+
+        // Ubah Collection menjadi array
+        $pendaftaranHarian = $pendaftaranHarian->all();
+
+        // Urutkan kembali array berdasarkan tanggal
+        usort($pendaftaranHarian, function ($a, $b) {
+            return strtotime($a->tanggal_pendaftaran) - strtotime($b->tanggal_pendaftaran);
+        });
+
+        $categories = [];
+        $pendaftaranData = [];
+
+        foreach ($pendaftaranHarian as $item) {
+            $categories[] = $item->tanggal_pendaftaran;
+            $pendaftaranData[] = $item->total_pendaftar;
+        }
+
+        $categoriesJson = json_encode($categories);
+        $pendaftaranDataJson = json_encode($pendaftaranData);
+
+        $data = [
+            'categories' => $categoriesJson,
+            'pendaftaranHarian' => $pendaftaranDataJson
+        ];
+
+        return $data;
+    }
+
+    public function pendaftaranBulanan()
+    {
+        $year = date('Y');
+        $month = date('m');
+
+        $pendaftarBulanan = DB::table('pendaftarans')
+            ->select(DB::raw("DATE_FORMAT(tglpendaftaran, '%Y-%m') AS bulan_pendaftaran"), DB::raw('COUNT(*) AS total_pendaftar'))
+            ->whereYear('tglpendaftaran', $year)
+            ->groupBy(DB::raw("DATE_FORMAT(tglpendaftaran, '%Y-%m')"))
+            ->orderBy('bulan_pendaftaran', 'ASC')
+            ->get();
+
+        // Inisialisasi array untuk menyimpan data yang akan digunakan di grafik Highcharts
+        $dataPendaftarBulanan = [];
+
+        // Mengisi array dataPendaftarBulanan dengan hasil query
+        foreach ($pendaftarBulanan as $result) {
+            $dataPendaftarBulanan[] = (int)$result->total_pendaftar;
+        }
+
+        return $dataPendaftarBulanan;
+    }
+
+    public function pendaftaranTahunan()
+    {
+        // Mendapatkan tahun pertama kali dan tahun terakhir dalam data
+        $firstYear = DB::table('pendaftarans')->min(DB::raw('YEAR(tglpendaftaran)'));
+        $lastYear = DB::table('pendaftarans')->max(DB::raw('YEAR(tglpendaftaran)'));
+
+        // Menambahkan 1 tahun ke tahun terakhir
+        $nextYear = $lastYear + 1;
+
+        // Query untuk menghitung jumlah pendaftaran per tahun dalam rentang tahun tersebut
+        $pendaftaranTahunan = DB::table('pendaftarans')
+            ->select(DB::raw("YEAR(tglpendaftaran) AS tahun_pendaftaran"), DB::raw('COALESCE(COUNT(*), 0) AS total_pendaftar'))
+            ->whereBetween(DB::raw('YEAR(tglpendaftaran)'), [$firstYear, $nextYear]) // Menggunakan $nextYear
+            ->groupBy(DB::raw("YEAR(tglpendaftaran)"))
+            ->orderBy('tahun_pendaftaran', 'ASC')
+            ->get();
+
+        // Tambahkan data manual untuk tahun berikutnya jika belum ada
+        $nextYearData = [
+            'tahun_pendaftaran' => $nextYear,
+            'total_pendaftar' => 0
+        ];
+
+        $pendaftaranTahunan->push($nextYearData);
+
+        return $pendaftaranTahunan;
     }
 }
--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -19,9 +19,39 @@
                 <div class="row">
                     <div class="col-12">
                         <div class="card">
+                            <div class="card-header">
+                                @php
+                                    \Carbon\Carbon::setLocale('id');
+                                @endphp
+                                <h4>Grafik Pendaftaran Harian, Bulan
+                                    @php
+                                        $date = $year . '-' . $month;
+                                    @endphp
+                                    {{ \Carbon\Carbon::parse($date)->translatedFormat('F Y') }}
+                                </h4>
+                            </div>
                             <div class="card-body">
-                                <table style="font-size: 14pt">
-                                </table>
+                                <div id="grafikHarian" class="" style="width: 100%; height:280px;"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <h4>Grafik Pendaftaran Tahun {{ $year }}</h4>
+                            </div>
+                            <div class="card-body">
+                                <div id="grafikBulanan" class="" style="width: 100%; height:280px;"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <h4>Grafik Pendaftaran per Tahun</h4>
+                            </div>
+                            <div class="card-body">
+                                <div id="grafikTahunan" class="" style="width: 100%; height:280px;"></div>
                             </div>
                         </div>
                     </div>
@@ -32,4 +62,139 @@
 @endsection
 
 @push('addons-js')
+    <script src="https://code.highcharts.com/highcharts.js"></script>
+    <script src="https://code.highcharts.com/modules/exporting.js"></script>
+
+    <script>
+        $(function() {
+            $('#grafikBulanan').highcharts({
+
+                title: {
+                    text: ""
+                },
+
+                xAxis: {
+                    categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt',
+                        'Nov', 'Dec'
+                    ],
+                    crosshair: true
+                },
+                yAxis: {
+                    min: 0,
+                    title: {
+                        text: 'Jumlah Pendaftaran Perbulan'
+                    }
+                },
+                tooltip: {
+                    headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+                    pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+                        '<td style="padding:0"><b> {point.y:1f} Pendaftaran</b></td></tr>',
+                    footerFormat: '</table>',
+                    shared: true,
+                    useHTML: true
+                },
+                plotOptions: {
+                    column: {
+                        pointPadding: 0.2,
+                        borderWidth: 0
+                    }
+                },
+                series: [{
+                    name: 'Pendaftaran',
+                    data: {{ $dataPendaftarBulanan }},
+                    color: "#2c5957",
+
+                }],
+                credits: "disabled"
+            });
+        });
+    </script>
+    <script>
+        $(function() {
+            var categories = <?php echo $datapendaftarHarianCategories; ?>;
+            var pendaftaranData = <?php echo $pendaftaranHarian; ?>;
+
+            $('#grafikHarian').highcharts({
+                title: {
+                    text: ""
+                },
+                xAxis: {
+                    categories: categories,
+                    crosshair: true
+                },
+                yAxis: {
+                    min: 0,
+                    title: {
+                        text: 'Jumlah Pendaftaran Perbulan'
+                    }
+                },
+                tooltip: {
+                    headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+                    pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+                        '<td style="padding:0"><b> {point.y:1f} Pendaftaran</b></td></tr>',
+                    footerFormat: '</table>',
+                    shared: true,
+                    useHTML: true
+                },
+                plotOptions: {
+                    column: {
+                        pointPadding: 0.2,
+                        borderWidth: 0
+                    }
+                },
+                series: [{
+                    name: 'Pendaftaran',
+                    data: pendaftaranData,
+                    color: "#2c5957",
+                }],
+                credits: {
+                    enabled: false
+                }
+            });
+        });
+    </script>
+
+    <script>
+        $(function() {
+            var data = <?php echo $dataPendaftarTahunan; ?>;
+
+            $('#grafikTahunan').highcharts({
+                title: {
+                    text: ""
+                },
+                xAxis: {
+                    categories: data.map(item => item.tahun_pendaftaran),
+                    crosshair: true
+                },
+                yAxis: {
+                    min: 0,
+                    title: {
+                        text: 'Jumlah Pendaftaran Perbulan'
+                    }
+                },
+                tooltip: {
+                    headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+                    pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+                        '<td style="padding:0"><b> {point.y:1f} Pendaftaran</b></td></tr>',
+                    footerFormat: '</table>',
+                    shared: true,
+                    useHTML: true
+                },
+                plotOptions: {
+                    column: {
+                        pointPadding: 0.2,
+                        borderWidth: 0
+                    }
+                },
+                series: [{
+                    name: 'Pendaftaran',
+                    data: data.map(item => item.total_pendaftar),
+                    color: "#2c5957",
+                }],
+                credits: {
+                    enabled: false
+                }
+            });
+        });
+    </script>
 @endpush
